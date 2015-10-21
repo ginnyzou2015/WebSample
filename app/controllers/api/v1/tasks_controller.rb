05c04a1c53c2c6e812002c0d9f957667ab787644
ginnyzou2015 <ginnyzou2015@gmail.com>
2015-10-20 21:35:15 -0400

    show recommendation

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,7 +10,8 @@ class Api::V1::TasksController < ApplicationController
   def index
     @email_id = params[:id] + ".com"
     @beacon_id = User.find_by_email(@email_id).beacon_id
-    @recommendation = Recommendation.where(:user => @email_id, :beacon => @beacon_id).first
+    @recommendation_record = Recommendation.where(:user => @email_id, :beacon => @beacon_id).first
+    @recommendation = @recommendation_record.recommendation
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in",
